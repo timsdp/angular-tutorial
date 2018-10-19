5c0677e8b77a23776ff12690e9966c9111ad0839
Tim <timreg@ua.fm>
2018-10-19 19:55:30 +0300

    Worked on ui

--- src/app/mock-products.ts
+++ src/app/mock-products.ts
@@ -1,14 +1,14 @@
 import { Product } from './product'
 
 export const PRODUCTS : Product[] = [
-    {id:11,name:"Cola",type:"Drink",energy:0},
-    {id:12,name:"Pepsi Black",type:"Drink",energy:0.4},
-    {id:13,name:"Fanta",type:"Drink",energy:0},
-    {id:14,name:"Sprite",type:"Drink",energy:0},
-    {id:15,name:"Beer",type:"Drink",energy:0},
-    {id:16,name:"Whiskey",type:"Drink",energy:0},
-    {id:17,name:"Tea",type:"Drink",energy:0},
-    {id:18,name:"Schweppes",type:"Drink",energy:0},
-    {id:19,name:"Water",type:"Drink",energy:0},
-    {id:20,name:"Coffee",type:"Drink",energy:0}
+    {id:11,name:"Cola",type:"Drink",energy:0,image:"cola-can.jpg"},
+    {id:12,name:"Pepsi Black",type:"Drink",energy:0.4,image:"pepsi-can.jpg"},
+    {id:13,name:"Fanta",type:"Drink",energy:0,image:null},
+    {id:14,name:"Sprite",type:"Drink",energy:0,image:null},
+    {id:15,name:"Beer",type:"Drink",energy:0,image:null},
+    {id:16,name:"Whiskey",type:"Drink",energy:0,image:null},
+    {id:17,name:"Tea",type:"Drink",energy:0,image:null},
+    {id:18,name:"Schweppes",type:"Drink",energy:0,image:null},
+    {id:19,name:"Water",type:"Drink",energy:0,image:null},
+    {id:20,name:"Coffee",type:"Drink",energy:0,image:null}
 ]
--- src/app/product.ts
+++ src/app/product.ts
@@ -3,4 +3,5 @@ export class Product {
     name: string;
     type: string;
     energy: number;
+    image:string;
   }
